ca81565d3770a9da6faf375548ef7b56373cda33
Lane Bretschneider <lanebret85@gmail.com>
2023-12-09 18:17:51 -0800

    feat: allow a User's image_path to be null and blank

--- boardgamebuddy_api/models.py
+++ boardgamebuddy_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    image_path = models.CharField(max_length=500)
+    image_path = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
