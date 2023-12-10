0275baf00e85ccf43093d7ce6a8cf608f6214b30
Connor Richmond <richmond.connor@gmail.com>
2023-12-10 15:27:16 -0500

    update tests to passing, no longer using pytest

--- boardgamebuddy_api/tests/test_models.py
+++ boardgamebuddy_api/tests/test_models.py
@@ -1,25 +1,16 @@
-import pytest
-from django.conf import settings
-from django.test import RequestFactory
-from django.db import IntegrityError
+from django.test import TestCase
 from boardgamebuddy_api.models import User, UserBoardGame
 
-@pytest.fixture
-def sample_user():
-    return User.objects.create(name='John Doe', email='john@example.com')
+class UserModelTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create(name="John Doe", email="john@example.com", image_path="example.jpg")
+        self.assertEqual(user.name, "John Doe")
+        self.assertEqual(user.email, "john@example.com")
+        self.assertEqual(user.image_path, "example.jpg")
 
-@pytest.fixture
-def sample_user_board_game(sample_user):
-    return UserBoardGame.objects.create(user=sample_user, board_game_id=123)
-
-def test_user_model():
-    user = User.objects.create(name='Test User', email='test@example.com', image_path='test.jpg')
-    assert str(user) == 'Test User'
-
-def test_user_board_game_model(sample_user_board_game):
-    user_board_game = sample_user_board_game
-    assert str(user_board_game) == f"{user_board_game.user.name}'s Favorite - BGID {user_board_game.board_game_id}"
-
-def test_user_board_game_unique_constraint(sample_user):
-    with pytest.raises(IntegrityError):
-        UserBoardGame.objects.create(user=sample_user, board_game_id=123)
+class UserBoardGameModelTest(TestCase):
+    def test_user_board_game_creation(self):
+        user = User.objects.create(name="John Doe", email="john@example.com", image_path="example.jpg")
+        user_board_game = UserBoardGame.objects.create(user=user, board_game_id=123)
+        self.assertEqual(user_board_game.user, user)
+        self.assertEqual(user_board_game.board_game_id, 123)
