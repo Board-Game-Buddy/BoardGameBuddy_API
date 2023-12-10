b1c74cce4d64703ce83d327238935053a268dfc0
Connor Richmond <richmond.connor@gmail.com>
2023-12-10 17:41:15 -0500

    test: facades

--- boardgamebuddy_api/tests/test_facades.py
+++ boardgamebuddy_api/tests/test_facades.py
@@ -1,15 +1,15 @@
-from django.test import TestCase
+import unittest
 from unittest.mock import MagicMock
 from boardgamebuddy_api.facades import BoardGamesFacade
 
-class BoardGamesFacadeTest(TestCase):
+class BoardGamesFacadeTest(unittest.TestCase):
     def setUp(self):
         self.board_games_service_mock = MagicMock()
         self.board_games_service_mock.get_board_games.return_value = [
-            {"id": 1, "name": "Game 1"},
-            {"id": 2, "name": "Game 2"},
+            {"id": 1, "attributes": {"title": "Game 1", "image_path": "/path/to/game1.jpg", "rank": 1}},
+            {"id": 2, "attributes": {"title": "Game 2", "image_path": "/path/to/game2.jpg", "rank": 2}},
         ]
-
+        
         self.facades = BoardGamesFacade([1, 2])
         self.facades.board_games_service = self.board_games_service_mock
 
@@ -20,6 +20,9 @@ class BoardGamesFacadeTest(TestCase):
 
         self.assertEqual(len(result), 2)
         self.assertEqual(result[0].id, 1)
-        self.assertEqual(result[0].name, "Game 1")
+        self.assertEqual(result[0].title, "Game 1")
         self.assertEqual(result[1].id, 2)
-        self.assertEqual(result[1].name, "Game 2")
+        self.assertEqual(result[1].title, "Game 2")
+
+if __name__ == '__main__':
+    unittest.main()
