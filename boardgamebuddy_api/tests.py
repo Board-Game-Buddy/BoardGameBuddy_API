b3eacdd376e9395126c0698d97966fbb448d2d07
Lane Bretschneider <lanebret85@gmail.com>
2023-12-10 17:09:01 -0800

    test: add tests for users post, user_details get, patch, and delete

--- boardgamebuddy_api/tests.py
+++ boardgamebuddy_api/tests.py
@@ -1,14 +1,10 @@
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import User
 from django.urls import path
 from boardgamebuddy_api import views
 
-class UserListAPITest(APITestCase, URLPatternsTestCase):
-	urlpatterns = [
-        path('users', views.user_list),
-    ]
-
+class UserListAPITest(APITestCase):
 	def test_user_list_api_get(self):
 		self.user1 = User.objects.create(name='testuser1', email='testuser1@example.com', image_path='testimage1')
 		self.user2 = User.objects.create(name='testuser2', email='testuser2@example.com', image_path='testimage2')
@@ -45,3 +41,90 @@ class UserListAPITest(APITestCase, URLPatternsTestCase):
 				}
 			}
 		])
+
+	def test_user_list_api_post(self):
+		data = {
+			"name": "testuser1",
+			"email": "testuser1@example.com",
+			"image_path": "testimage1"
+		}
+		url = '/users'
+		response = self.client.post(url, data, format='json')
+
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+		response_data = response.json()
+
+		self.assertEqual(response_data, {
+			"data": {
+				"id": 1,
+				"type": "user",
+				"attributes": {
+					"name": "testuser1",
+					"email": "testuser1@example.com",
+					"image_path": "testimage1"
+				}
+			}
+		})
+
+	class UserDetailsAPITest(APITestCase):
+		def test_user_details_api_get(self):
+			self.user1 = User.objects.create(name='testuser1', email='testuser1@example.com', image_path='testimage1')
+
+			url = '/users/1'
+			response = self.client.get(url, format='json')
+
+			self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+			data = response.json()
+
+			self.assertEqual(data, {
+				"data": {
+					"id": 1,
+					"type": "user",
+					"attributes": {
+						"name": "testuser1",
+						"email": "testuser1@example.com",
+						"image_path": "testimage1"
+					}
+				}
+			})
+
+		def test_user_details_api_patch(self):
+			self.user1 = User.objects.create(name='testuser1', email='testuser1@example.com', image_path='testimage1')
+
+			data = {
+				"name": "testuser1_edit",
+				"email": "testuser1@example.com",
+				"image_path": "testimage1"
+			}
+			url = '/users/1'
+			response = self.client.patch(url, data, format='json')
+
+			self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+			response_data = response.json()
+
+			self.assertEqual(response_data, {
+				"data": {
+					"id": 1,
+					"type": "user",
+					"attributes": {
+						"name": "testuser1_edit",
+						"email": "testuser1@example.com",
+						"image_path": "testimage1"
+					}
+				}
+			})
+
+		def test_user_details_api_delete(self):
+			self.user1 = User.objects.create(name='testuser1', email='testuser1@example.com', image_path='testimage1')
+
+			url = '/users/1'
+			response = self.client.patch(url, format='json')
+
+			self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+			response_data = response.json()
+
+			self.assertEqual(response_data, "User Deleted")
